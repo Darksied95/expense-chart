78d7d3c2b94e0f294da7f1036b1888bbf598d7e0
Darksied95 <aliyuefe9501@gmail.com>
2022-09-07 02:33:30 +0100

    final touches added, bugs fixed

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import data from "./assets/data.json";
 import BarChart from "./components/BarChart.jsx";
 
 function App() {
-  const totalAmountFromData = data.reduce((acc, cv) => acc + cv.amount, 0);
-
   const barStyles = {
     height: "76px",
     width: "40px",
@@ -15,14 +13,20 @@ function App() {
 
   function calculateBarStyles(height, id) {
     const getCurrentDate = new Date().getDay();
-    const newHeight = ((height * 300) / totalAmountFromData) * 2.5;
-    return +id === getCurrentDate
-      ? {
-          ...barStyles,
-          height: `${newHeight}px`,
-          backgroundColor: "var(--clr-cyan)",
-        }
-      : { ...barStyles, height: `${newHeight}px` };
+    const highestAmountFromData = Math.max(...data.map((each) => each.amount));
+    const multiplier = 250 / highestAmountFromData;
+    const newHeight = multiplier * height;
+
+    //The aove calculation makes sure the max height of any bar is equal to 250
+    if (+id === getCurrentDate) {
+      return {
+        ...barStyles,
+        height: `${newHeight}px`,
+        backgroundColor: "var(--clr-cyan)",
+      };
+    } else {
+      return { ...barStyles, height: `${newHeight}px` };
+    }
   }
   return (
     <main>
